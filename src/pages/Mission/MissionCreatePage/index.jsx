7a1b87f3afd583b780059e3aa15090a6eaaf72ec
JieunKim <78676660+mellykim123@users.noreply.github.com>
2023-09-08 09:18:03 +0900

    Revert "feat: mission create add react hook form"
    This reverts commit 1a0baed3e37163ae43682386c66f896ba51df991.

--- src/pages/Mission/MissionCreatePage/index.jsx
+++ src/pages/Mission/MissionCreatePage/index.jsx
@@ -1,27 +1,3 @@
-import { Path, useForm, UseFormRegister } from 'react-hook-form'
-
-const Input = ({ label, register, required }) => (
-  <>
-    <label>{label}</label>
-    <input {...register(label, { required })} />
-  </>
-)
-
 export const MissionCreatePage = () => {
-  const { register, handleSubmit } = useForm()
-
-  const onSubmit = data => {
-    alert(JSON.stringify(data))
-  }
-
-  return (
-    <div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Input label="Child Name" register={register} required />
-        <Input label="Mission Title" register={register} required />
-        <Input label="Reward" register={register} required />
-        <input type="submit" />
-      </form>
-    </div>
-  )
+  return <div>Mission Create Page</div>
 }
