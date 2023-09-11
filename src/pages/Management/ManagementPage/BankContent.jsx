5a83a9010981879aacfe17833ad889d431dc8cc7
songhee <96781855+songhee1@users.noreply.github.com>
2023-09-11 16:11:02 +0900

    feat : axios update

--- src/pages/Management/ManagementPage/BankContent.jsx
+++ src/pages/Management/ManagementPage/BankContent.jsx
@@ -6,6 +6,7 @@ import c2 from './img/아이2.jpg';
 import c3 from './img/아이3.jpg';
 import c4 from './img/아이4.jpg';
 import axios from 'axios';
+const token = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJwYXJlbnQyQGdtYWlsLmNvbSIsImlhdCI6MTY5NDQxMDYxMiwiZXhwIjoxNjk3MDAyNjEyfQ.REnYwc1UCodiCGsXPRNiTPq8cCUSBQP_On95izs_c54";
 
 const BankContentContainer = styled.div`
   width: 100%;
@@ -53,17 +54,34 @@ padding:10px;
 const BankContent = ({ selectedPicture, setSelectedPicture }) => {
   const [childName, setChildName] = useState("");
   const [accountNum, setAccountNum] = useState("");
+  const [childId, setChildId] = useState(2);
   const controls = useAnimation();
 
-  useEffect(() => {
-    // // selectedPicture가 변경될 때마다 애니메이션 적용
-    // controls.start({ opacity: 0, transition: { duration: 0.5 } });
-    // setTimeout(() => {
-    //   setSelectedPicture(selectedPicture);
-    // }, 500); // 애니메이션 지속 시간 이후에 다시 초기 이미지로 설정
 
-    axios
-    .get('/management/send')
+ 
+
+  useEffect(() => {
+       // 이미지 경로에 따라 c1, c2, c3, c4로 대체
+    if (selectedPicture === "./img/아이1.jpeg") {
+      setSelectedPicture(c1);
+      setChildId(2);
+    } else if (selectedPicture === "./img/아이2.jpg") {
+      setSelectedPicture(c2);
+      setChildId(4);
+    } else if (selectedPicture === "./img/아이3.jpg") {
+      setSelectedPicture(c3);
+      setChildId(6);
+    } else if (selectedPicture === "./img/아이4.jpg") {
+      setSelectedPicture(c4);
+      setChildId(7);
+    }
+
+    var url = '/management/'+childId;
+    axios({
+      url: url,
+      method : "get",
+      headers : {Authorization: token}
+    })
     .then((response) => { // axios then 호출
       if(response.data){
         console.log('axios 확인');
@@ -79,25 +97,31 @@ const BankContent = ({ selectedPicture, setSelectedPicture }) => {
     .finally(() => {
     });
 
+  
+
   }, [selectedPicture]);
 
-  // 이미지 경로에 따라 c1, c2, c3, c4로 대체
-  if (selectedPicture === "./img/아이1.jpeg") {
-    selectedPicture = c1;
-  } else if (selectedPicture === "./img/아이2.jpg") {
-    selectedPicture = c2;
-  } else if (selectedPicture === "./img/아이3.jpg") {
-    selectedPicture = c3;
-  } else if (selectedPicture === "./img/아이4.jpg") {
-    selectedPicture = c4;
-  }
-
+  
   return (
     <BankContentContainer>
       <ChildName>{childName != "" ? <span>{childName}님의 계좌</span> : ""}</ChildName>
       <PictureSelectContainer>
         {selectedPicture == null ? (
-          <div id="comment">송금하실 자녀를 선택해주세요</div>
+          <motion.img
+          initial={{ opacity: 0 }} // 초기 상태
+          animate={{ opacity: 1 }} // 애니메이션 적용
+          exit={{ opacity: 0 }} // 나가기 애니메이션 설정
+          transition={{ duration: 0.5 }} // 애니메이션 지속 시간 설정
+          key={selectedPicture} // 이미지 변경 시에 컴포넌트를 새로 렌더링하기 위한 키 설정
+          src={c1}
+          alt="preview-img"
+          className="show"
+          style={{
+            objectFit: 'cover', // 이미지를 컨테이너에 맞게 크기 조정하고 비율 유지
+            width: '100%', // 컨테이너 너비에 맞게 이미지 가로 크기 조정
+            height: '100%', // 컨테이너 높이에 맞게 이미지 세로 크기 조정
+          }}
+        />
         ) : (
           <div>
             
