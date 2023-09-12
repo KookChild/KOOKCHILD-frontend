09e3617a5abfedf2f199fc44e0e64ce6daccbd03
yoojung.choi <green980611@naver.com>
2023-09-12 15:27:41 +0900

    feat: login ui & api

---
+++ src/apis/login/index.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://127.0.0.1:8080';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+axios.defaults.headers.put['Content-Type'] = 'application/json';
+
+if (localStorage.getItem('token')) {
+  axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}`;
+}
+
+export const loginAPI = async (email, password) => {
+  try {
+      const response = await axios.post("/login", {
+          email: email,
+          password: password
+      });
+
+      const token = response.data.token;
+
+      // Token을 Axios 헤더에 설정
+      axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+
+      // 토큰을 로컬 스토리지에 저장
+      localStorage.setItem('token', token);
+
+      return token;
+  } catch (error) {
+      console.error("Login error", error);
+      throw error;
+  }
+};
--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -1,3 +1,62 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import {
+  CenteredContainer,
+  Header,
+  HeaderContent,
+  HeaderImage,
+  HeaderTitle,
+  Form,
+  Input,
+  Button
+} from './style';
+import imgSrc3 from './prefer.png';
+import { loginAPI } from '../../apis/login/index';
+
 export const LoginPage = () => {
-  return <div> Login Page</div>
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const token = await loginAPI(email, password);
+      console.log("Token received:", token);
+      navigate('/info');
+    } catch (error) {
+      console.error("Login failed", error);
+    }
+  };
+
+  return (
+    <div>
+      <CenteredContainer>
+        <Header>
+          <HeaderContent>
+            <HeaderImage src={imgSrc3} />
+            <HeaderTitle>로그인</HeaderTitle>
+          </HeaderContent>
+        </Header>
+        <Form onSubmit={handleSubmit}>
+      <Input
+        type="text"
+        placeholder="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+      />
+      <Input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        required
+      />
+      <Button type="submit">Login</Button>
+    </Form>
+      </CenteredContainer>
+    </div>
+  );
 }
