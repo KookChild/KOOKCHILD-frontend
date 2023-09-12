c021adc11fed054561de70aa2dd7954daf00b3d4
JieunKim <78676660+mellykim123@users.noreply.github.com>
2023-09-12 17:28:00 +0900

    add: child page mission api

--- src/pages/Main/ChildMainPage/index.jsx
+++ src/pages/Main/ChildMainPage/index.jsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { loadAllMissionAPI, loadChildAccountDetailAPI } from '@apis'
+import { getMissions, loadChildAccountDetailAPI } from '@apis'
 import {
   headerContainer,
   iconContainer,
@@ -26,16 +26,16 @@ library.add(faBell, faGear, faPlus)
 
 export const ChildMainPage = () => {
   const [child, setChildData] = useState({
-    accountName: "",
-    accountNum: "",
-    balance: "",
+    accountName: '',
+    accountNum: '',
+    balance: '',
   })
   const [missionList, setMissionList] = useState([])
   const handleCopyClick = () => {
     navigator.clipboard.writeText('553702-01-000000')
   }
 
-  const [animatedDigits, setAnimatedDigits] = useState([]);
+  const [animatedDigits, setAnimatedDigits] = useState([])
 
   const challengesArray = [
     { id: 1, title: '26주 적금 도전', imageURL: sampleChallenge, progress: 50 },
@@ -51,74 +51,77 @@ export const ChildMainPage = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const childDataResponse = await loadChildAccountDetailAPI();
-        let formattedBalance;
-
+        const childDataResponse = await loadChildAccountDetailAPI()
+        let formattedBalance
+        const missionDataResponse = await getMissions('newest')
         // childDataResponse.balance의 타입에 따라 적절하게 처리
         if (typeof childDataResponse.balance === 'number') {
-          formattedBalance = childDataResponse.balance.toLocaleString();
+          formattedBalance = childDataResponse.balance.toLocaleString()
         } else if (typeof childDataResponse.balance === 'string') {
-          formattedBalance = childDataResponse.balance;
+          formattedBalance = childDataResponse.balance
         } else {
-          console.error('Balance is neither a number nor a string:', childDataResponse.balance);
-          formattedBalance = 'N/A'; // Not Available로 표시
+          console.error(
+            'Balance is neither a number nor a string:',
+            childDataResponse.balance,
+          )
+          formattedBalance = 'N/A' // Not Available로 표시
         }
 
         setChildData({
           accountName: childDataResponse.accountName,
           accountNum: childDataResponse.accountNum,
           balance: formattedBalance, // 처리된 문자열로 저장
-        });
+        })
+        setMissionList(missionDataResponse.missionLists)
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error('Error fetching data:', error)
       }
-    };
+    }
 
-    fetchData();
-  }, []);
-
+    fetchData()
+  }, [])
 
   useEffect(() => {
-    if (child.balance === null) return;
-    let balanceStr = String(child.balance); // 문자열로 변환
-    let digitsArray = balanceStr.split(''); // 이제 child.balance는 문자열이므로 split이 가능
-  
+    if (child.balance === null) return
+    let balanceStr = String(child.balance) // 문자열로 변환
+    let digitsArray = balanceStr.split('') // 이제 child.balance는 문자열이므로 split이 가능
+
     // animatedDigitsTemp 배열의 초기 길이를 확인하고,
     // 필요한 경우 animatedDigits 배열의 길이를 조정합니다.
     if (animatedDigits.length !== balanceStr.length) {
-      let lengthDifference = balanceStr.length - animatedDigits.length;
-  
+      let lengthDifference = balanceStr.length - animatedDigits.length
+
       // 자릿수가 늘었다면 앞에 '0'을 추가합니다.
       if (lengthDifference > 0) {
-        let newZeros = Array.from({ length: lengthDifference }, () => '0');
-        setAnimatedDigits([...newZeros, ...animatedDigits]);
+        let newZeros = Array.from({ length: lengthDifference }, () => '0')
+        setAnimatedDigits([...newZeros, ...animatedDigits])
       }
       // 자릿수가 줄었다면 앞의 '0'을 제거합니다.
       else if (lengthDifference < 0) {
-        setAnimatedDigits(animatedDigits.slice(-lengthDifference));
+        setAnimatedDigits(animatedDigits.slice(-lengthDifference))
       }
     }
-  
-    let animatedDigitsTemp = [...animatedDigits]; // 현재의 animatedDigits를 복사
-  
+
+    let animatedDigitsTemp = [...animatedDigits] // 현재의 animatedDigits를 복사
+
     digitsArray.forEach((digit, index) => {
       let animation = setInterval(() => {
-        const randomDigit = Math.floor(Math.random() * 10);
-        animatedDigitsTemp[index] = randomDigit.toString();
-  
+        const randomDigit = Math.floor(Math.random() * 10)
+        animatedDigitsTemp[index] = randomDigit.toString()
+
         // 전체 배열을 사용하여 상태를 업데이트
-        setAnimatedDigits([...animatedDigitsTemp]);
-      }, 25);
-  
+        setAnimatedDigits([...animatedDigitsTemp])
+      }, 25)
+
       setTimeout(() => {
-        clearInterval(animation);
-        animatedDigitsTemp[index] = digit;
-  
+        clearInterval(animation)
+        animatedDigitsTemp[index] = digit
+
         // 전체 배열을 사용하여 상태를 업데이트
-        setAnimatedDigits([...animatedDigitsTemp]);
-      }, 100 + index * 150);
-    });
-  }, [child.balance]); // child.balance가 변경될 때마다 이 효과를 적용
+        setAnimatedDigits([...animatedDigitsTemp])
+      }, 100 + index * 150)
+    })
+  }, [child.balance]) // child.balance가 변경될 때마다 이 효과를 적용
 
   return (
     <div style={headerContainer}>
--- src/pages/Mission/MissionChildViewPage/index.jsx
+++ src/pages/Mission/MissionChildViewPage/index.jsx
@@ -1,55 +1,61 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { MissionItem, BackHeader } from '@components';
-import { BodyContainer, Dropdown, MissionListContainer } from './style';
-import { getMissions } from '../../../apis/mission/index';
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { MissionItem, BackHeader } from '@components'
+import { BodyContainer, Dropdown, MissionListContainer } from './style'
+import { getMissions } from '@apis'
 
 export const MissionChildViewPage = () => {
-    const [missions, setMissions] = useState([]);
-    const [sort, setSort] = useState("newest");
-    const navigate = useNavigate(); // <-- 여기에 추가
+  const [missions, setMissions] = useState([])
+  const [sort, setSort] = useState('newest')
+  const navigate = useNavigate() // <-- 여기에 추가
 
-    const handleMissionClick = (missionId) => {
-        navigate(`/mission/detail/${missionId}`); // <-- 여기를 수정
-    };
-    useEffect(() => {
-        const fetchData = async () => {
-            const data = await getMissions(sort);
-            console.log(data.missionLists);
-            setMissions(data.missionLists);
-        }
-        fetchData();
-    }, [sort]);
+  const handleMissionClick = missionId => {
+    navigate(`/mission/detail/${missionId}`) // <-- 여기를 수정
+  }
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getMissions(sort)
+      console.log(data.missionLists)
+      setMissions(data.missionLists)
+    }
+    fetchData()
+  }, [sort])
 
-    const handleDropdownChange = (e) => {
-        const selectedValue = e.target.value;
-        setSort(selectedValue);
-    };
+  const handleDropdownChange = e => {
+    const selectedValue = e.target.value
+    setSort(selectedValue)
+  }
 
-
-    return (
-        <div>
-            <BodyContainer>
-            <BackHeader text="미션 목록" onBackClick={() => navigate('/path-to-go-back')} />
-                <Dropdown onChange={handleDropdownChange}>
-                    <option value="newest">최신순</option>
-                    <option value="oldest">오래된순</option>
-                </Dropdown>
-                <MissionListContainer>
-                {missions.map((mission, index) => (
-                    <div onClick={() => handleMissionClick(mission.id)} key={index}>
-                <MissionItem
-                    isEven={index % 2 === 0}
-                    missionTitle={mission.title}
-                    missionReward={`${mission.reward}원`}
-                    missionDate={mission.deadline !== 'null-null' ? mission.deadline : '기한 없음'}
-                    parentConfirm={mission.parentConfirm}
-                    childConfirm={mission.childConfirm}
-                />
-                    </div>
-                ))}
-            </MissionListContainer>
-            </BodyContainer>
-        </div>
-    );
-};
+  return (
+    <div>
+      <BodyContainer>
+        <BackHeader
+          text="미션 목록"
+          onBackClick={() => navigate('/path-to-go-back')}
+        />
+        <Dropdown onChange={handleDropdownChange}>
+          <option value="newest">최신순</option>
+          <option value="oldest">오래된순</option>
+        </Dropdown>
+        <MissionListContainer>
+          {missions.map((mission, index) => (
+            <div onClick={() => handleMissionClick(mission.id)} key={index}>
+              <MissionItem
+                isEven={index % 2 === 0}
+                missionTitle={mission.title}
+                missionReward={`${mission.reward}원`}
+                missionDate={
+                  mission.deadline !== 'null-null'
+                    ? mission.deadline
+                    : '기한 없음'
+                }
+                parentConfirm={mission.parentConfirm}
+                childConfirm={mission.childConfirm}
+              />
+            </div>
+          ))}
+        </MissionListContainer>
+      </BodyContainer>
+    </div>
+  )
+}
