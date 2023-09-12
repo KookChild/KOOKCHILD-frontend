67ebf102de3cd519b7497bc18271f7b879b97f61
KSEOP <kts9335@naver.com>
2023-09-12 14:54:45 +0900

    Refactor: 자녀 메인 잔액 효과 수정

--- src/pages/Main/ChildMainPage/index.jsx
+++ src/pages/Main/ChildMainPage/index.jsx
@@ -1,8 +1,7 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { loadAllMissionAPI, loadChildAccountDetailAPI } from '@apis'
-import { useEffect, useState } from 'react'
 import {
   headerContainer,
   iconContainer,
@@ -19,7 +18,7 @@ import {
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faBell, faGear, faPlus } from '@fortawesome/free-solid-svg-icons'
 // ChallengeItem.jsx
-import { ChallengeItem } from './ChallengeItem'
+import { ChallengeItem } from '@components'
 // 샘플 이미지 테스트
 import sampleChallenge from './img/Sample_challenge.jpg'
 //
@@ -27,20 +26,16 @@ library.add(faBell, faGear, faPlus)
 
 export const ChildMainPage = () => {
   const [child, setChildData] = useState({
-    accountName: '김국민',
-    accountNum: 0,
-    balance: 0,
+    accountName: "",
+    accountNum: "",
+    balance: "",
   })
   const [missionList, setMissionList] = useState([])
   const handleCopyClick = () => {
     navigator.clipboard.writeText('553702-01-000000')
   }
 
-  const missionsArray = [
-    { title: '미션명1', detail: '미션 상세내용1', reward: 2000 },
-    { title: '미션명2', detail: '미션 상세내용2', reward: 3000 },
-    { title: '미션명3', detail: '미션 상세내용2', reward: 4000 },
-  ]
+  const [animatedDigits, setAnimatedDigits] = useState([]);
 
   const challengesArray = [
     { id: 1, title: '26주 적금 도전', imageURL: sampleChallenge, progress: 50 },
@@ -56,22 +51,75 @@ export const ChildMainPage = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const childDataResponse = await loadChildAccountDetailAPI()
-        const missionListResponse = await loadAllMissionAPI()
-        const formattedBalance = childDataResponse.balance.toLocaleString()
+        const childDataResponse = await loadChildAccountDetailAPI();
+        let formattedBalance;
+
+        // childDataResponse.balance의 타입에 따라 적절하게 처리
+        if (typeof childDataResponse.balance === 'number') {
+          formattedBalance = childDataResponse.balance.toLocaleString();
+        } else if (typeof childDataResponse.balance === 'string') {
+          formattedBalance = childDataResponse.balance;
+        } else {
+          console.error('Balance is neither a number nor a string:', childDataResponse.balance);
+          formattedBalance = 'N/A'; // Not Available로 표시
+        }
+
         setChildData({
           accountName: childDataResponse.accountName,
           accountNum: childDataResponse.accountNum,
-          balance: formattedBalance,
-        })
-        setMissionList(missionListResponse)
+          balance: formattedBalance, // 처리된 문자열로 저장
+        });
       } catch (error) {
-        console.error('Error fetching data:', error)
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+
+  useEffect(() => {
+    if (child.balance === null) return;
+    let balanceStr = String(child.balance); // 문자열로 변환
+    let digitsArray = balanceStr.split(''); // 이제 child.balance는 문자열이므로 split이 가능
+  
+    // animatedDigitsTemp 배열의 초기 길이를 확인하고,
+    // 필요한 경우 animatedDigits 배열의 길이를 조정합니다.
+    if (animatedDigits.length !== balanceStr.length) {
+      let lengthDifference = balanceStr.length - animatedDigits.length;
+  
+      // 자릿수가 늘었다면 앞에 '0'을 추가합니다.
+      if (lengthDifference > 0) {
+        let newZeros = Array.from({ length: lengthDifference }, () => '0');
+        setAnimatedDigits([...newZeros, ...animatedDigits]);
+      }
+      // 자릿수가 줄었다면 앞의 '0'을 제거합니다.
+      else if (lengthDifference < 0) {
+        setAnimatedDigits(animatedDigits.slice(-lengthDifference));
       }
     }
+  
+    let animatedDigitsTemp = [...animatedDigits]; // 현재의 animatedDigits를 복사
+  
+    digitsArray.forEach((digit, index) => {
+      let animation = setInterval(() => {
+        const randomDigit = Math.floor(Math.random() * 10);
+        animatedDigitsTemp[index] = randomDigit.toString();
+  
+        // 전체 배열을 사용하여 상태를 업데이트
+        setAnimatedDigits([...animatedDigitsTemp]);
+      }, 25);
+  
+      setTimeout(() => {
+        clearInterval(animation);
+        animatedDigitsTemp[index] = digit;
+  
+        // 전체 배열을 사용하여 상태를 업데이트
+        setAnimatedDigits([...animatedDigitsTemp]);
+      }, 100 + index * 150);
+    });
+  }, [child.balance]); // child.balance가 변경될 때마다 이 효과를 적용
 
-    fetchData()
-  }, [])
   return (
     <div style={headerContainer}>
       <div style={iconContainer}>
@@ -98,7 +146,7 @@ export const ChildMainPage = () => {
           </div>
           <br />
           <div style={{ fontSize: '22px', cursor: 'pointer' }}>
-            {child.balance}
+            {animatedDigits.join('')}
           </div>
           <br />
           <Link to="/transaction-history" style={CustomLinkButton}>
--- src/pages/Management/ManagementPage/index.jsx
+++ src/pages/Management/ManagementPage/index.jsx
@@ -148,7 +148,7 @@ export const ManagementPage = () => {
 
 
     
-  }), [childId];
+  }, [childId]);
 
 
 
